70a28ed70ef16d75905a9128e1a6c97564fb4179
Ahmed Sereta <seretaahmed18@gmail.com>
2026-05-13 21:52:43 +0300

    added routes

--- futureProject/urls.py
+++ futureProject/urls.py
@@ -1,22 +1,29 @@
 """
-URL configuration for futureProject project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
+
+from futureApp.serializers import CustomTokenObtainPairView
+from futureApp.views import UserViewSet, UserInfoView, ChangePasswordView
+from futureProject import settings
+
+# Create router and register viewSets
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    # JWT Authentication
+    path('api/gettoken/', CustomTokenObtainPairView.as_view(), name='gettoken'),
+    path('api/refresh_token/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('api/verify_token/', TokenVerifyView.as_view(), name='verify_token'),
+    path('api/userinfo/', UserInfoView.as_view(), name='userinfo'),
+    path('api/userinfo/change-password/', ChangePasswordView.as_view(), name="change-password"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
